8f5305fd7070de0b5787514565f93a392c0ac8b8
Vytautas Perliba <vytautas.per@gmail.com>
2016-10-15 12:57:57 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+angular.module('HackSheffield', ['ui.bootstrap','ngRoute','ngAnimate']);
+
+angular.module('HackSheffield').config(function($routeProvider) {
+
+    $routeProvider.when('/home', {
+        templateUrl: 'libs/partial/home.html'
+    });
+
+    /* Add New Routes Above */
+    $routeProvider.otherwise({redirectTo:'/home'});
+
+});
+
+angular.module('HackSheffield').run(function($rootScope) {
+
+    $rootScope.safeApply = function(fn) {
+        var phase = $rootScope.$$phase;
+        if (phase === '$apply' || phase === '$digest') {
+            if (fn && (typeof(fn) === 'function')) {
+                fn();
+            }
+        } else {
+            this.$apply(fn);
+        }
+    };
+
+});
---
+++ libs/partial/home.js
@@ -0,0 +1,4 @@
+angular.module('HackSheffield').controller('HomeCtrl',function($scope){
+
+
+});
