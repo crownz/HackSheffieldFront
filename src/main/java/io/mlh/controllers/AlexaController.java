75d75a383bbb840d0239da5ba0205358256ab3d1
Mantas <mantasakalauskas@gmail.com>
2016-10-15 18:46:56 +0100

    Add alexa controller logging

--- src/main/java/io/mlh/controllers/AlexaController.java
+++ src/main/java/io/mlh/controllers/AlexaController.java
@@ -1,5 +1,7 @@
 package io.mlh.controllers;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/alexa")
 public class AlexaController {
 
+    private final static Logger logger = LogManager.getLogger(AlexaController.class);
+
     public AlexaController() {
 
     }
 
     @RequestMapping("/setMode")
     public void setNewMode(@RequestParam String displayMode) {
-
+        logger.error("Called alexa setMode!");
     }
 
     //Request type
