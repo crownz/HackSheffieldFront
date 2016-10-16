9a8bbe77092407e9473003d6f6e6b665e3857630
Mantas <mantasakalauskas@gmail.com>
2016-10-16 08:47:57 +0100

    Add proper camelcasing

--- src/main/java/io/mlh/utilities/WordUtils.java
+++ src/main/java/io/mlh/utilities/WordUtils.java
@@ -7,11 +7,13 @@ public class WordUtils {
     }
 
     public static String toCamelCase(String s){
-        String[] parts = s.split("_");
+        String[] parts = s.split(" ");
         String camelCaseString = "";
         for (String part : parts){
             camelCaseString = camelCaseString + toProperCase(part);
         }
+
+        camelCaseString = camelCaseString.substring(0, 1).toLowerCase() + camelCaseString.substring(1, camelCaseString.length());
         return camelCaseString;
     }
 
