8e5a7b668683040cda254b347a9c88899243ab07
Mantas <mantasakalauskas@gmail.com>
2016-10-16 08:39:30 +0100

    Add wordutils

---
+++ src/main/java/io/mlh/utilities/WordUtils.java
@@ -0,0 +1,22 @@
+package io.mlh.utilities;
+
+public class WordUtils {
+
+    private WordUtils() {
+
+    }
+
+    public static String toCamelCase(String s){
+        String[] parts = s.split("_");
+        String camelCaseString = "";
+        for (String part : parts){
+            camelCaseString = camelCaseString + toProperCase(part);
+        }
+        return camelCaseString;
+    }
+
+    static String toProperCase(String s) {
+        return s.substring(0, 1).toUpperCase() +
+                s.substring(1).toLowerCase();
+    }
+}
